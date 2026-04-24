74407a618cf9a1e851b3afb39c8cebd1c1a67e46
Ayush Srivastava <constayush@gmail.com>
2026-04-24 17:01:04 +0530

    added power-cycle btn on waybar

--- scripts/power-cycle.sh
+++ scripts/power-cycle.sh
@@ -3,7 +3,35 @@
 CURRENT=$(powerprofilesctl get)
 
 case $CURRENT in
-  performance) powerprofilesctl set balanced ;;
-  balanced) powerprofilesctl set power-saver ;;
-  power-saver) powerprofilesctl set performance ;;
+  performance)
+    powerprofilesctl set balanced
+    ;;
+  balanced)
+    powerprofilesctl set power-saver
+    ;;
+  power-saver)
+    powerprofilesctl set performance
+    ;;
 esac
+
+# Get updated state AFTER switching
+NEW=$(powerprofilesctl get)
+
+# Map icons (optional but cleaner UX)
+case $NEW in
+  performance)
+    ICON="⚡"
+    MSG="Performance Mode"
+    ;;
+  balanced)
+    ICON="🔋"
+    MSG="Balanced Mode"
+    ;;
+  power-saver)
+    ICON="🌱"
+    MSG="Power Saver Mode"
+    ;;
+esac
+
+# Send notification
+notify-send "Power Profile Changed" "$ICON $MSG"
